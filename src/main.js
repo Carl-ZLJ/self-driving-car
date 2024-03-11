1f488292dd51c7210ca161f086691e9cf610df0b
Carl-ZLJ <zljmms32@gmail.com>
2024-03-11 13:17:44 +0800

    prettier

--- src/main.js
+++ src/main.js
@@ -1,105 +1,105 @@
-const e = (el) => document.querySelector(el);
+const e = (el) => document.querySelector(el)
 
-const carCanvas = e("#carCanvas");
-carCanvas.width = 200;
-const networkCanvas = e("#networkCanvas");
-networkCanvas.width = 300;
+const carCanvas = e('#carCanvas')
+carCanvas.width = 200
+const networkCanvas = e('#networkCanvas')
+networkCanvas.width = 300
 
-const carCtx = carCanvas.getContext("2d");
-const networkCtx = networkCanvas.getContext("2d");
-const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
-const N = 1000;
-const cars = generateCars(N);
+const carCtx = carCanvas.getContext('2d')
+const networkCtx = networkCanvas.getContext('2d')
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9)
+const N = 1000
+const cars = generateCars(N)
 
-let bestCar = cars[0];
+let bestCar = cars[0]
 
-if (localStorage.getItem("bestBrain")) {
-  for (let i = 0; i < cars.length; i++) {
-    cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
-    if (i !== 0) {
-      NeuralNetwork.mutate(cars[i].brain, 0.2);
+if (localStorage.getItem('bestBrain')) {
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].brain = JSON.parse(localStorage.getItem('bestBrain'))
+        if (i !== 0) {
+            NeuralNetwork.mutate(cars[i].brain, 0.2)
+        }
     }
-  }
 }
 
 const traffic = [
-  new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
-  new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
-  new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2),
-  new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2),
-  new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2),
-  new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2),
-  new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2),
-];
+    new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2),
+    new Car(road.getLaneCenter(0), -300, 30, 50, 'DUMMY', 2),
+    new Car(road.getLaneCenter(2), -300, 30, 50, 'DUMMY', 2),
+    new Car(road.getLaneCenter(0), -500, 30, 50, 'DUMMY', 2),
+    new Car(road.getLaneCenter(1), -500, 30, 50, 'DUMMY', 2),
+    new Car(road.getLaneCenter(1), -700, 30, 50, 'DUMMY', 2),
+    new Car(road.getLaneCenter(2), -700, 30, 50, 'DUMMY', 2),
+]
 
 function saveBestCar() {
-  localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
+    localStorage.setItem('bestBrain', JSON.stringify(bestCar.brain))
 }
 
 function discard() {
-  localStorage.removeItem("bestBrain");
+    localStorage.removeItem('bestBrain')
 }
 
 function generateCars(N) {
-  const cars = [];
-  for (let i = 1; i <= N; i++) {
-    cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI"));
-  }
-  return cars;
+    const cars = []
+    for (let i = 1; i <= N; i++) {
+        cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, 'AI'))
+    }
+    return cars
 }
 
 function findBestCar(cars, fitnessFunc) {
-  // what is the best car ?
-  let bestCar = cars.find(fitnessFunc);
+    // what is the best car ?
+    let bestCar = cars.find(fitnessFunc)
 
-  return bestCar;
+    return bestCar
 }
 
 function animate(time) {
-  for (let i = 0; i < traffic.length; i++) {
-    traffic[i].update(road.borders, []);
-  }
+    for (let i = 0; i < traffic.length; i++) {
+        traffic[i].update(road.borders, [])
+    }
 
-  for (let i = 0; i < cars.length; i++) {
-    cars[i].update(road.borders, traffic);
-  }
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].update(road.borders, traffic)
+    }
 
-  bestCar = findBestCar(
-    cars,
-    (car) => car.y == Math.min(...cars.map((c) => c.y))
-  );
+    bestCar = findBestCar(
+        cars,
+        (car) => car.y == Math.min(...cars.map((c) => c.y))
+    )
 
-  // refresh the canvas
-  carCanvas.height = window.innerHeight;
-  networkCanvas.height = window.innerHeight;
+    // refresh the canvas
+    carCanvas.height = window.innerHeight
+    networkCanvas.height = window.innerHeight
 
-  carCtx.save();
-  carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7);
+    carCtx.save()
+    carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7)
 
-  road.draw(carCtx);
+    road.draw(carCtx)
 
-  for (let i = 0; i < traffic.length; i++) {
-    traffic[i].draw(carCtx, "blue");
-  }
+    for (let i = 0; i < traffic.length; i++) {
+        traffic[i].draw(carCtx, 'blue')
+    }
 
-  carCtx.globalAlpha = 0.2;
-  for (let i = 0; i < cars.length; i++) {
-    cars[i].draw(carCtx, "black");
-  }
-  carCtx.globalAlpha = 1;
+    carCtx.globalAlpha = 0.2
+    for (let i = 0; i < cars.length; i++) {
+        cars[i].draw(carCtx, 'black')
+    }
+    carCtx.globalAlpha = 1
 
-  bestCar.draw(carCtx, "black", true);
+    bestCar.draw(carCtx, 'black', true)
 
-  carCtx.restore();
+    carCtx.restore()
 
-  networkCtx.lineDashOffset = -time / 60;
-  Visualizer.drawNetwork(networkCtx, bestCar.brain);
+    networkCtx.lineDashOffset = -time / 60
+    Visualizer.drawNetwork(networkCtx, bestCar.brain)
 
-  requestAnimationFrame(animate);
+    requestAnimationFrame(animate)
 }
 
 function main() {
-  animate();
+    animate()
 }
 
-main();
+main()
--- src/visualizer.js
+++ src/visualizer.js
@@ -1,110 +1,106 @@
 class Visualizer {
-  static drawNetwork(ctx, network) {
-    const margin = 50
-    const left = margin
-    const top = margin
-    const width = ctx.canvas.width - margin * 2
-    const height = ctx.canvas.height - margin * 2
+    static drawNetwork(ctx, network) {
+        const margin = 50
+        const left = margin
+        const top = margin
+        const width = ctx.canvas.width - margin * 2
+        const height = ctx.canvas.height - margin * 2
 
-    const levelHeight = height / network.levels.length
-    for (let i = network.levels.length - 1; i >= 0; i--) {
-      const levelTop = top + lerp(
-        height - levelHeight,
-        0,
-        network.levels.length == 1 ?
-          0.5 : i / (network.levels.length - 1)
-      )
+        const levelHeight = height / network.levels.length
+        for (let i = network.levels.length - 1; i >= 0; i--) {
+            const levelTop =
+                top +
+                lerp(
+                    height - levelHeight,
+                    0,
+                    network.levels.length == 1
+                        ? 0.5
+                        : i / (network.levels.length - 1)
+                )
 
-      ctx.setLineDash([7, 3])
-      Visualizer.drawLevel(ctx, network.levels[i],
-        left, levelTop,
-        width, levelHeight,
-        i == network.levels.length - 1 ? ['⬆️', '⬅️', '➡️', '⬇️'] : []
-      )
-
-    }
-  }
-
-  static drawLevel(ctx, level, left, top, width, height, outputLabels) {
-    const right = left + width
-    const bottom = top + height
-
-    const nodeRadius = 18
-
-    const { inputs, outputs, weights, biases } = level
-    const ilen = inputs.length
-    const olen = outputs.length
-
-    for (let i = 0; i < ilen; i++) {
-      for (let j = 0; j < olen; j++) {
-        ctx.beginPath()
-        ctx.moveTo(
-          Visualizer.#getNodeX(inputs, i, left, right),
-          bottom,
-        )
-        ctx.lineTo(
-          Visualizer.#getNodeX(outputs, j, left, right),
-          top,
-        )
-        ctx.lineWidth = 2
-        ctx.strokeStyle = getRGBA(weights[i][j])
-        ctx.stroke()
-      }
+            ctx.setLineDash([7, 3])
+            Visualizer.drawLevel(
+                ctx,
+                network.levels[i],
+                left,
+                levelTop,
+                width,
+                levelHeight,
+                i == network.levels.length - 1 ? ['⬆️', '⬅️', '➡️', '⬇️'] : []
+            )
+        }
     }
 
-    for (let i = 0; i < ilen; i++) {
-      const x = Visualizer.#getNodeX(inputs, i, left, right)
+    static drawLevel(ctx, level, left, top, width, height, outputLabels) {
+        const right = left + width
+        const bottom = top + height
 
-      ctx.beginPath()
-      ctx.arc(x, bottom, nodeRadius, 0, Math.PI * 2)
-      ctx.fillStyle = 'black'
-      ctx.fill()
+        const nodeRadius = 18
 
-      ctx.beginPath()
-      ctx.arc(x, bottom, nodeRadius * 0.6, 0, Math.PI * 2)
-      ctx.fillStyle = getRGBA(inputs[i])
-      ctx.fill()
+        const { inputs, outputs, weights, biases } = level
+        const ilen = inputs.length
+        const olen = outputs.length
+
+        for (let i = 0; i < ilen; i++) {
+            for (let j = 0; j < olen; j++) {
+                ctx.beginPath()
+                ctx.moveTo(Visualizer.#getNodeX(inputs, i, left, right), bottom)
+                ctx.lineTo(Visualizer.#getNodeX(outputs, j, left, right), top)
+                ctx.lineWidth = 2
+                ctx.strokeStyle = getRGBA(weights[i][j])
+                ctx.stroke()
+            }
+        }
+
+        for (let i = 0; i < ilen; i++) {
+            const x = Visualizer.#getNodeX(inputs, i, left, right)
+
+            ctx.beginPath()
+            ctx.arc(x, bottom, nodeRadius, 0, Math.PI * 2)
+            ctx.fillStyle = 'black'
+            ctx.fill()
+
+            ctx.beginPath()
+            ctx.arc(x, bottom, nodeRadius * 0.6, 0, Math.PI * 2)
+            ctx.fillStyle = getRGBA(inputs[i])
+            ctx.fill()
+        }
+
+        for (let i = 0; i < olen; i++) {
+            const x = Visualizer.#getNodeX(outputs, i, left, right)
+            ctx.beginPath()
+            ctx.arc(x, top, nodeRadius, 0, Math.PI * 2)
+            ctx.fillStyle = 'black'
+            ctx.fill()
+            ctx.beginPath()
+            ctx.arc(x, top, nodeRadius * 0.6, 0, Math.PI * 2)
+            ctx.fillStyle = getRGBA(outputs[i])
+            ctx.fill()
+
+            ctx.beginPath()
+            ctx.lineWidth = 2
+            ctx.arc(x, top, nodeRadius, 0, Math.PI * 2)
+            ctx.strokeStyle = getRGBA(biases[i])
+            ctx.setLineDash([3, 3])
+            ctx.stroke()
+            ctx.setLineDash([])
+
+            if (outputLabels[i]) {
+                ctx.beginPath()
+                ctx.textAlign = 'center'
+                ctx.textBaseline = 'middle'
+                ctx.fillStyle = 'black'
+                ctx.strokeStyle = 'white'
+                ctx.font = 'bold 12px Arial'
+                ctx.fillText(outputLabels[i], x, top)
+                ctx.lineWidth = 0.5
+                ctx.strokeText(outputLabels[i], x, top)
+            }
+        }
     }
 
-    for (let i = 0; i < olen; i++) {
-      const x = Visualizer.#getNodeX(outputs, i, left, right)
-      ctx.beginPath()
-      ctx.arc(x, top, nodeRadius, 0, Math.PI * 2)
-      ctx.fillStyle = 'black'
-      ctx.fill()
-      ctx.beginPath()
-      ctx.arc(x, top, nodeRadius * 0.6, 0, Math.PI * 2)
-      ctx.fillStyle = getRGBA(outputs[i])
-      ctx.fill()
-
-      ctx.beginPath()
-      ctx.lineWidth = 2
-      ctx.arc(x, top, nodeRadius, 0, Math.PI * 2)
-      ctx.strokeStyle = getRGBA(biases[i])
-      ctx.setLineDash([3, 3])
-      ctx.stroke()
-      ctx.setLineDash([])
-
-      if (outputLabels[i]) {
-        ctx.beginPath()
-        ctx.textAlign = 'center'
-        ctx.textBaseline = 'middle'
-        ctx.fillStyle = 'black'
-        ctx.strokeStyle = 'white'
-        ctx.font = 'bold 12px Arial'
-        ctx.fillText(outputLabels[i], x, top)
-        ctx.lineWidth = 0.5
-        ctx.strokeText(outputLabels[i], x, top)
-      }
+    static #getNodeX(nodes, index, left, right) {
+        const l = nodes.length
+        return lerp(left, right, l == 1 ? 0.5 : index / (l - 1))
     }
-  }
-
-  static #getNodeX(nodes, index, left, right) {
-    const l = nodes.length
-    return lerp(
-      left,
-      right,
-      l == 1 ? 0.5 : index / (l - 1),
-    )
-  }
 }
